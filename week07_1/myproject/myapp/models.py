fc502401a6531275b4462b908a2a9860b9ba5370
wasit@vision <wasit7@gmail.com>
2018-03-07 15:03:03 +0700

    after class week07_1

--- week07_1/myproject/myapp/models.py
+++ week07_1/myproject/myapp/models.py
@@ -5,11 +5,15 @@ class Car(models.Model):
 	model=models.CharField(max_length=100)
 	detail=models.CharField(max_length=100)
 	price=models.DecimalField(max_digits=10, decimal_places=2)
+	def __str__(self):
+		return "id: %s, model: %s, price: %s"%(self.id, self.model, self.price)
 
 class Customer(models.Model):
 	first_name=models.CharField(max_length=100)
 	last_name=models.CharField(max_length=100)
 	phone=models.CharField(max_length=20)
+	def __str__(self):
+		return "id: {}, {}".format(self.id, self.first_name)
 
 class Rent(models.Model):
 	car_id=models.ForeignKey('Car',on_delete=models.CASCADE)
