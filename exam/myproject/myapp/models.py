e21dba8d75794642fcd1e3e8faef275911605e22
Wasit Limprasert <wasit7@gmail.com>
2018-05-09 14:02:45 +0700

    quiz

--- exam/myproject/myapp/models.py
+++ exam/myproject/myapp/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class Room(models.Model):
 	number = models.CharField(max_length=255)
 	hour_price = models.DecimalField(max_digits=10, decimal_places=2)
+	isavailable = models.BooleanField(default=True)
 
 class Member(models.Model):
 	name = models.CharField(max_length=255)
@@ -12,6 +13,6 @@ class Member(models.Model):
 class Rent(models.Model):
 	room = models.ForeignKey(Room, on_delete=models.SET_NULL, blank=True,null=True)
 	member = models.ForeignKey(Member, on_delete=models.SET_NULL, blank=True,null=True)
-	start = models.DateTimeField( default=timezone.now )
-	stop = models.DateTimeField( default=timezone.now )
+	start = models.DateTimeField( )
+	stop = models.DateTimeField( null=True, blank=True )
 	cost = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
